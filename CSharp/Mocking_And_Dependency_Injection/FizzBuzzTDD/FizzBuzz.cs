c4cdeb8060409aa5c1e30c7b87b3c6a4146e527c
mefish <matt.e.fisher@gmail.com>
2017-06-08 10:13:35 -0400

    Created mocking and dependency injection

---
+++ CSharp/Mocking_And_Dependency_Injection/FizzBuzzTDD/FizzBuzz.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzzTDD
+{
+    public class FizzBuzz
+    {
+        private const string FIZZTEXT = "Fizz";
+        private const string BUZZTEXT = "Buzz";
+        private const int BUZZMULTIPLE = 5;
+        private const int FIZZMULTIPLE = 3;
+        private string _replacementText;
+
+        public string GetFizzAndOrBuzz(int numberToConvert)
+        {
+            ClearReplacementText();
+
+            AddFizzIfFizz(numberToConvert);
+            AddBuzzIfBuzz(numberToConvert);
+
+            if (AnyReplacementText()) return _replacementText;
+
+            return numberToConvert.ToString();
+        }
+
+        private void AddBuzzIfBuzz(int numberToConvert)
+        {
+            if (IsBuzz(numberToConvert)) _replacementText += BUZZTEXT;
+        }
+
+        private void AddFizzIfFizz(int numberToConvert)
+        {
+            if (IsFizz(numberToConvert)) _replacementText += FIZZTEXT;
+        }
+
+        private void ClearReplacementText()
+        {
+            _replacementText = string.Empty;
+        }
+
+        private bool AnyReplacementText()
+        {
+            return _replacementText != string.Empty;
+        }
+
+        private static bool IsBuzz(int startNumber)
+        {
+            return startNumber % BUZZMULTIPLE == 0;
+        }
+
+        private static bool IsFizz(int startNumber)
+        {
+            return startNumber % FIZZMULTIPLE == 0;
+        }
+    }
+}
---
+++ CSharp/Mocking_And_Dependency_Injection/FizzBuzzTDD/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzzTDD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fizzBuzz = new FizzBuzz();
+            for (int i = 1; i < 101; i++)
+            {
+                Console.WriteLine(fizzBuzz.GetFizzAndOrBuzz(i));
+            }
+            Console.WriteLine("Press 'Enter' to exit");
+            while(!(Console.KeyAvailable && Console.ReadKey(true).Key == ConsoleKey.Enter))
+            {
+                
+            }
+        }
+    }
+}
