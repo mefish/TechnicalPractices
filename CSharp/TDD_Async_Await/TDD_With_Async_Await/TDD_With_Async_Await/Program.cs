396170cd0be96f0785c54fa386fd91284362abc3
mefish <matt.e.fisher@gmail.com>
2017-07-17 14:20:16 -0400

    basic console implementation

--- CSharp/TDD_Async_Await/TDD_With_Async_Await/TDD_With_Async_Await/Program.cs
+++ CSharp/TDD_Async_Await/TDD_With_Async_Await/TDD_With_Async_Await/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TDD_With_Async_Await_Common;
 
 namespace TDD_With_Async_Await
 {
@@ -10,7 +11,51 @@ namespace TDD_With_Async_Await
     {
         static void Main(string[] args)
         {
-            //This simple project makes a "GET" call to a "Database" and prints the output
+            Console.Title = typeof(Program).Name;
+
+            Run();
+        }
+
+        private static void Run()
+        {
+            while (true)
+            {
+                var consoleInput = ReadFromConsole();
+                if (string.IsNullOrWhiteSpace(consoleInput)) continue;
+
+                try
+                {
+                    string result = Execute(consoleInput);
+
+                    WriteToConsole(result);
+                }
+                catch (Exception exception)
+                {
+                    WriteToConsole(exception.Message);
+                }
+            }
+        }
+
+        private const string READ_PROMPT = "console> ";
+        private static string ReadFromConsole(string promptMessage = "")
+        {
+            Console.WriteLine(READ_PROMPT + promptMessage);
+            return Console.ReadLine();
+        }
+
+        private static void WriteToConsole(string message = "")
+        {
+            if (message.Length > 0) Console.WriteLine(message);
+        }
+
+        private static string Execute(string command)
+        {
+            return $"Executed the {command} command";
+        }
+
+        public void DisplayToUser(string stringToDisplay)
+        {
+            Console.WriteLine(stringToDisplay);
         }
     }
 }
